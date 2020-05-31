93c514bc8d3ebb538b10a90c7a63280d34f08fea
Musfir Ahmed <musfir.ahmed@cleartrip.com>
2020-05-31 18:46:04 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMeal::Application.config.secret_token = '0c146fb3d56917838935904e41b68cb47b386408b22d2b155bbd9d1d0c1c43c7fac8d62e551fd6f2fd3148c8f3fc32f39e8b206479d2d0406ff3d1a716b9ce35'
